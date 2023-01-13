e9c8d1e85d73bff44efeb8efedb32f591cd90fb0
ahadkhaan97 <ahad.sartaj.ew@gmail.com>
2023-01-13 16:47:52 +0500

    one more case covered

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,21 @@
 fun main() {
-    print(solution("qqq"))
+    print(solution("AAAAAA"))
 }
 
 fun solution(inputString: String): Boolean {
+
     val map = HashMap<Char, Int>()
 
+    var count = 0
+
     for (i in inputString.indices) {
+        if (inputString[i].isUpperCase())
+            count++
         map[inputString[i]] = (map[inputString[i]] ?: 0) + 1
     }
+
+    if (count != inputString.length && inputString[0].isUpperCase() && inputString[inputString.length - 1].isUpperCase())
+        return false
 
     map.forEach {
         if (it.value % 2 != 0)
